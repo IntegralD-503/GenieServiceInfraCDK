8880e748bc9a0cc7fa7e8b8f3236f9f07918dcaa
IntegralD-503 <hunter.clark@utexas.edu>
2023-02-03 03:25:28 -0500

    Initial commit

---
+++ test/genie_service.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as GenieService from '../lib/genie_service-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/genie_service-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new GenieService.GenieServiceStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
