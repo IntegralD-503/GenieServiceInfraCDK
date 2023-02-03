849fe4b5cfdb8a99e73a62d7c88fab56dde063c8
IntegralD-503 <hunter.clark@utexas.edu>
2023-02-03 03:46:45 -0500

    pipeline skeleton part 1

---
+++ pipeline/pipeline-stack.ts
@@ -0,0 +1,110 @@
+import { SecretValue, Stack, StackProps } from "aws-cdk-lib";
+import { PipelineProject, LinuxBuildImage, BuildSpec } from "aws-cdk-lib/aws-codebuild";
+import { Artifact, IStage, Pipeline } from "aws-cdk-lib/aws-codepipeline";
+import { CloudFormationCreateUpdateStackAction, CodeBuildAction, GitHubSourceAction } from "aws-cdk-lib/aws-codepipeline-actions";
+import { Construct } from "constructs";
+import { GenieServiceStack } from "../lib/genie_service-stack";
+
+export class PipelineStack extends Stack {
+    private readonly pipeline: Pipeline;
+    private readonly cdkBuildOutput: Artifact;
+    private readonly genieServiceSourceOutput: Artifact;
+    private readonly genieServiceBuildOutput: Artifact;
+
+    constructor(scope: Construct, id: string, props?: StackProps) {
+        super(scope, id, props);
+
+
+        this.pipeline = new Pipeline(this, 'GenieServicePipeline', {
+            pipelineName: 'GenieServicePipeline',
+            crossAccountKeys: false,
+            restartExecutionOnUpdate: true
+          });
+      
+          const cdkSourceOutput = new Artifact('GenieServiceCDKOutput');
+          this.genieServiceSourceOutput = new Artifact('GenieServiceSourceOutput');
+      
+          this.pipeline.addStage({
+            stageName:'Source',
+            actions:[
+              new GitHubSourceAction({
+                owner: 'IntegralD-503',
+                repo: 'GenieServiceInfraCDK',
+                branch: 'master',
+                actionName: 'GenieServicePipeline_Source',
+                oauthToken: SecretValue.secretsManager('github-token'),
+                output: cdkSourceOutput
+              }),
+              new GitHubSourceAction({
+                owner: 'IntegralD-503',
+                repo: 'GenieService',
+                branch: 'main',
+                actionName: 'GenieService_Source',
+                oauthToken: SecretValue.secretsManager('github-token'),
+                output: this.genieServiceSourceOutput
+              })
+            ]
+          });
+
+          this.cdkBuildOutput = new Artifact("CdkBuildOutput");
+          this.genieServiceBuildOutput = new Artifact("GenieServiceBuildOutput");
+      
+          this.pipeline.addStage({
+            stageName: 'Build',
+            actions: [
+              new CodeBuildAction({
+                actionName: 'CDK_Build',
+                input: cdkSourceOutput,
+                outputs: [ this.cdkBuildOutput ],
+                project: new PipelineProject(this, 'CdkBuildProject', {
+                  environment: {
+                    buildImage: LinuxBuildImage.AMAZON_LINUX_2_4
+                  },
+                  buildSpec: BuildSpec.fromSourceFilename('build-specs/cdk-build-spec.yml')
+                })
+              }),
+              new CodeBuildAction({
+                actionName: 'ModelHubAPI_Build',
+                input: this.genieServiceSourceOutput,
+                outputs: [ this.genieServiceBuildOutput ],
+                project: new PipelineProject(this, 'GenieServiceBuildProject', {
+                  environment: {
+                    buildImage: LinuxBuildImage.AMAZON_LINUX_2_4
+                  },
+                  buildSpec: BuildSpec.fromSourceFilename('build-specs/genei-service-build-spec.yml')
+                })
+              })
+            ]
+          });
+
+          this.pipeline.addStage({
+            stageName: 'Pipeline_Update',
+            actions: [
+              new CloudFormationCreateUpdateStackAction({
+                actionName: 'GenieServicePipeline_Update',
+                stackName: 'GenieServicePipelineStack',
+                templatePath: this.cdkBuildOutput.atPath('GenieServiceStack.template.json'),
+                adminPermissions: true,
+              })
+            ]
+          });
+    }
+
+    public addGenieServiceStage(genieStack: GenieServiceStack, stageName: string): IStage {
+      return this.pipeline.addStage({
+        stageName: stageName,
+        actions: [
+          new CloudFormationCreateUpdateStackAction({
+            actionName: 'GenieService_Update',
+            stackName: genieStack.stackName,
+            templatePath: this.cdkBuildOutput.atPath(`${genieStack.stackName}.template.json`),
+            adminPermissions: true,
+            parameterOverrides: {
+              ...genieStack.serviceCode.assign(this.genieServiceBuildOutput.s3Location)
+            },
+            extraInputs: [this.genieServiceBuildOutput]
+          })
+        ]
+      })
+    }
+}
