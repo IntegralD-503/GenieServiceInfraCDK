30c69b1e7ecea81debb8c91668b5d09c5e4e91c0
IntegralD-503 <hunter.clark@utexas.edu>
2023-02-08 19:39:24 -0500

    update tests

--- bin/genie_service.ts
+++ bin/genie_service.ts
@@ -5,7 +5,7 @@ import { GenieServiceStack } from '../lib/genie_service-stack';
 import { PipelineStack } from '../pipeline/pipeline-stack';
 
 const app = new cdk.App();
-const pipeline = new PipelineStack(app, "GenieServicePiplineStack")
+const pipeline = new PipelineStack(app, "GenieServicePipelineStack")
 const genieService = new GenieServiceStack(app, 'GenieServiceStack', {});
 
 pipeline.addGenieServiceStage(genieService, "Beta")
