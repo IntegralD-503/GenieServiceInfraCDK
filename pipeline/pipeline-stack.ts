de9ba1cbd2886ae2cd254cdb4c4875b19ba7d2f7
IntegralD-503 <hunter.clark@utexas.edu>
2023-02-08 21:32:41 -0500

    fix auth error??

--- pipeline/pipeline-stack.ts
+++ pipeline/pipeline-stack.ts
@@ -2,6 +2,7 @@ import { SecretValue, Stack, StackProps } from "aws-cdk-lib";
 import { PipelineProject, LinuxBuildImage, BuildSpec } from "aws-cdk-lib/aws-codebuild";
 import { Artifact, IStage, Pipeline } from "aws-cdk-lib/aws-codepipeline";
 import { CloudFormationCreateUpdateStackAction, CodeBuildAction, EcsDeployAction, GitHubSourceAction } from "aws-cdk-lib/aws-codepipeline-actions";
+import { Effect, PolicyDocument, PolicyStatement, Role, ServicePrincipal } from "aws-cdk-lib/aws-iam";
 import { Construct } from "constructs";
 import { GenieServiceStack } from "../lib/genie_service-stack";
 
@@ -46,9 +47,30 @@ export class PipelineStack extends Stack {
             ]
           });
 
+          
+
           this.cdkBuildOutput = new Artifact("CdkBuildOutput");
           this.genieServiceBuildOutput = new Artifact("GenieServiceBuildOutput");
-      
+    
+          const codeBuildECRTokenAccess = new PolicyDocument({
+            statements: [
+              new PolicyStatement({
+                effect: Effect.ALLOW,
+                resources: ['*'],
+                actions: [
+                          "ecr:GetAuthorizationToken",
+                      ]
+              })
+            ]
+          });
+
+          const codeBuildECRTokenAccessRole = new Role(this, "GenieServiceBuildActionECRRole", {
+            assumedBy: new ServicePrincipal("codebuild.amazonaws.com"),
+            inlinePolicies: {
+             CodeBuildECRTokenAccess: codeBuildECRTokenAccess
+            }
+          })
+
           this.pipeline.addStage({
             stageName: 'Build',
             actions: [
@@ -72,7 +94,8 @@ export class PipelineStack extends Stack {
                     buildImage: LinuxBuildImage.AMAZON_LINUX_2_4
                   },
                   buildSpec: BuildSpec.fromSourceFilename('build-specs/genie-service-build-spec.yml')
-                })
+                }),
+                role: codeBuildECRTokenAccessRole
               })
             ]
           });
